0a903d3d62b5e0b59d90d6fc10e095f0c809e6d0
Lymei <lymeicontato@gmail.com>
2023-06-08 16:31:06 -0300

    fix: fixed date input do not saving it self value

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,50 +1,74 @@
 'use client';
 
 import EventsType from '@/types/events';
+import Inputs from '@/types/inputs';
 import { formData } from '@/utils/const';
 import dayjs from 'dayjs';
-import { useState, useRef } from 'react';
+import { useState, useRef, useMemo } from 'react';
 //@ts-ignore
 import CalendarHeatmap from 'react-calendar-heatmap';
 import 'react-calendar-heatmap/dist/styles.css';
+import { useForm, SubmitHandler } from 'react-hook-form';
 
 function Calendar() {
 	let yearly = dayjs().subtract(365, 'days').format('YYYY-MM-DD');
 
 	const initialDate: EventsType[] = [
-		{ date: '2023-03-01', count: 12, data: { type: 'constante', location: 'queixo', medication: 'Nenhuma' } },
+		{
+			date: '2023-03-01',
+			count: 12,
+			data: [{ type: 'constante', location: 'queixo', medication: 'Nenhuma' }],
+		},
 	];
-
-	// TODO: Arrumar date input value e onChange rerender error
 
 	const [events, setEvents] = useState<EventsType[]>(initialDate);
 	const [isAddingEvent, setIsAddingEvent] = useState(false);
 	const [locationSelected, setLocationSelected] = useState<string>();
-	const dateSelected = useRef();
+	const [dateSelected, setDateSelected] = useState<string>();
 
-	const addingCancel = () => {
-		setIsAddingEvent(false);
+	const getTodayDate = () => {
+		const date = new Date();
+		return `${date.getFullYear()}-${(date.getMonth() + 1).toString().padStart(2, '0')}-${date
+			.getDate()
+			.toString()
+			.padStart(2, '0')}`;
 	};
 
-	const addEvent = () => {
-		if (locationSelected && dateSelected) {
-			setEvents((data) => [
-				...data,
-				{
-					date: '2023-05-01',
-					count: 12,
-					data: { type: 'constante', location: locationSelected, medication: 'Nenhuma' },
-				},
+	const {
+		register,
+		handleSubmit,
+		setValue,
+		formState: { errors },
+	} = useForm<EventsType>({ defaultValues: { date: getTodayDate() } });
+	const onSubmit: SubmitHandler<EventsType> = (data) => addEvent(data);
+
+	const addingEventFinish = () => {
+		setIsAddingEvent(false);
+		setValue('date', getTodayDate());
+	};
+
+	const addEvent = (data: EventsType) => {
+		data.count = 1;
+		console.log(data);
+		if (events.find((event) => event.date == data.date)) {
+			setEvents((events) => [
+				...events.map((event) => {
+					if (event.date == data.date) {
+						event.count++;
+						event.data.push(...data.data);
+					}
+					return event;
+				}),
 			]);
-			console.log(locationSelected);
-			console.log(dateSelected);
-			setIsAddingEvent(false);
-		}
+		} else setEvents((events) => [...events, data]);
+		addingEventFinish();
 	};
 
 	const AddMenu = () => (
 		<div className='absolute flex justify-center items-center inset-0 backdrop-blur-sm'>
-			<div className='w-1/3 h-2/3 flex flex-col items-center gap-8 border border-black bg-white dark:border-white dark:bg-black rounded-md p-4'>
+			<form
+				onSubmit={handleSubmit(onSubmit)}
+				className='w-1/3 h-2/3 flex flex-col items-center gap-8 border border-black bg-white dark:border-white dark:bg-black rounded-md p-4'>
 				<p className='text-2xl'>Adicionar Evento</p>
 				<div className='flex flex-col gap-8'>
 					<div className='flex flex-col gap-2 justify-center'>
@@ -52,9 +76,8 @@ function Calendar() {
 						<input
 							className='p-2 border border-black text-black dark:bg-black dark:text-white dark:border-white rounded-md outline-none'
 							type='date'
-							name='date'
 							autoComplete='off'
-							ref={dateSelected}
+							{...register('date', { required: true })}
 						/>
 					</div>
 					<div className='flex flex-col items-center gap-4'>
@@ -66,10 +89,8 @@ function Calendar() {
 									<input
 										id={data + index}
 										value={data}
-										name='location'
-										checked={locationSelected == data ? true : false}
-										onChange={() => setLocationSelected(data)}
 										type='radio'
+										{...register('data.0.location', { required: true })}
 									/>
 								</div>
 							))}
@@ -77,18 +98,17 @@ function Calendar() {
 					</div>
 				</div>
 				<div className='flex gap-4'>
-					<button
-						onClick={() => {
-							addEvent();
-						}}
-						className='p-2 border border-black dark:border-white rounded-md'>
+					<button className='p-2 border border-black dark:border-white rounded-md' type='submit'>
 						Adicionar
 					</button>
-					<button onClick={addingCancel} className='p-2 border border-black dark:border-white rounded-md'>
+					<button
+						onClick={addingEventFinish}
+						className='p-2 border border-black dark:border-white rounded-md'
+						type='button'>
 						Cancelar
 					</button>
 				</div>
-			</div>
+			</form>
 		</div>
 	);
 
